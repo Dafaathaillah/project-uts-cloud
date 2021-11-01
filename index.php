1fc94a4d6f794d0bee51ae48c9c8dea31fd36748
Dafa Bintang Athaillah <Dafabintang76@gmail.com>
2021-11-01 12:05:39 +0700

    update link href

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/frmj1jkdkjnm/b/Dafaathaillah/o/uts-cloud%2Fcss%2Fbootstrap.css" rel="stylesheet">
     <title>Sistem Pendaftaran</title>
 </head>
     <?php include "header.php"; ?>
@@ -21,8 +21,8 @@
             Silahkan pilih <b>Menu Daftar Baru</b> untuk melakukan pendaftaran
         </p>
     </div>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/popper.min.js"></script>
+    <script src="https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/frmj1jkdkjnm/b/Dafaathaillah/o/uts-cloud%2Fjs%2Fbootstrap.js"></script>
+    <script src="https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/frmj1jkdkjnm/b/Dafaathaillah/o/uts-cloud%2Fjs%2Fpopper.min.js"></script>
 </body>
 
 </html>
